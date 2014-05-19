07cd784149eb95e42f34e1eed7b0dc563764c6bc
Sasha <bobrova.aleksand@gmail.com>
2014-05-19 23:07:05 +0400

    It's already done

--- Integral/main.cpp
+++ Integral/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include "matrix.h"
 
 #define pi (3.141592653589793)
+#define M_2PI (2.*pi)
 //result = 3.57886
 
 double w_f (double, vector < double > );
@@ -20,13 +22,14 @@ void findRootsCardano(const vector<double>, vector <double> &);
 
 int main(){
 	//cout<<Gauss(0.1, 2.3);
-	cout << calculateIntegralG(0.1, 2.3, 3);
+	
+	cout << setprecision(12)<< calculateIntegralG(0.1, 2.3, AitkenG(0.1, 2.3));
 	//cout << NewtonCotes(0.1, 2.3);
 	//cout << calculateIntegralNC(0.1, 2.3, AitkenNC(0.1, 2.3)) << endl;
 	system("pause");
 	return 0;
 }
-double f ( double x ){
+double f ( const double x ){
 	double res = 2.5*cos(2 * x)*exp(2 * x / 3) + 4 * sin(3.5*x)*exp(-3 * x) + 3 * x;
 	if (fabs(res) < EPS){
 		res = 0.0;
@@ -47,16 +50,13 @@ vector <double> calculateMomentsG(double a, double b){
 	moments.resize(6);
 	moments[0] = 1.250 *(pow(b - 0.10, 0.80) - pow(a - 0.10, 0.80));
 	moments[1] = 0.0110062 * ((1 + 8 * b) *pow(-1 + 10 * b, 0.8) -  (1 + 8 * a) * pow(-1 + 10 * a, 0.8));
-	moments[2] = pow(-0.1 + b, 0.8) * ((0.00496032 + (0.0396825 + 0.357143 * b) * b) -
-				 (0.00496032 + (0.0396825 + 0.357143 * a) * a));
-	moments[3] = 0.0000620651 * pow(-1 + 10 * b, 0.8) *((1 + 8 * b + 72 * pow(b, 2) + 672 * pow(b, 3)) - 
-		(1 + 8 * a + 72 * pow(a, 2) + 672 * pow(a, 3)));
-	moments[4] = 0.00012413 * pow(-1 + 10 * b, 0.8) * ((0.0416667 + b/3 + 3*pow(b, 2) + 28*pow( b , 3) + 266 *pow(b, 4)) -
-		 (0.0416667 + a / 3 + 3 * pow(a, 2) + 28 * pow(a, 3) + 266 * pow(a, 4)));
-	moments[5] = 0.0000299624 * pow( - 1.0 + 10.0 * b, 0.80) * ((0.014881 + 0.119048*b + 1.07143 * pow(b, 2) +
-		10 * pow(b, 3) + 95 * pow(b, 4) + 912 * pow(b, 5)) - (0.014881 + 0.119048*a + 1.07143 * pow(a, 2) +
-		10 * pow(a, 3) + 95 * pow(a, 4) + 912 * pow(a, 5)));
-
+	moments[2] = 0.357143 * pow(-0.1 + b, 0.80)*(0.0138889 + b / 9 + pow(b, 2)) - 0.357143 * pow(-0.1 + a, 0.80)*(0.0138889 + a / 9 + pow(a, 2));
+	moments[3] = 0.0726174 * pow(-0.5 + 5. * b, 0.80)* (0.0014881 + 0.0119048*b + 0.107143 * pow(b, 2) + 1.*pow(b, 3)) -
+		0.0726174 * pow(-0.5 + 5. * a, 0.80)* (0.0014881 + 0.0119048*a + 0.107143 * pow(a, 2) + 1.*pow(a, 3));
+	moments[4] = 0.0574887*pow(-0.5 + 5.* b, 0.80) *(0.000156642 + 0.00125313 * b + 0.0112782*pow(b, 2) + 0.105263*pow(b, 3) + 1. * pow(b, 4)) -
+		0.0574887*pow(-0.5 + 5.* a, 0.80) *(0.000156642 + 0.00125313 * a + 0.0112782*pow(a, 2) + 0.105263*pow(a, 3) + 1. * pow(a, 4));
+	moments[5] = 0.0475769 * pow(-0.5 + 5. * b, 0.80) * (0.0000163168 + 0.000130535 * b + 0.00117481*pow(b, 2) + 0.0109649*pow(b, 3) + 0.104167*pow(b, 4) + 1.*pow(b, 5)) -
+		0.0475769 * pow(-0.5 + 5. * a, 0.80) * (0.0000163168 + 0.000130535 * a + 0.00117481*pow(a, 2) + 0.0109649*pow(a, 3) + 0.104167*pow(a, 4) + 1.*pow(a, 5));
 	return moments;
 }
 double Gauss (const double l, const double r){
@@ -64,13 +64,15 @@ double Gauss (const double l, const double r){
 	vector <double> M;
 	M.resize(6);
 	copy(M, calculateMomentsG(l, r));
-
+	checkEps(M);
+	//cout << "Moments: " << endl;
+	//print(M);
 	//решаем слау
 	vector <double> mns;
 	for (int i = 0; i < 3; ++i){
 		mns.push_back(-M[i + 3]);
 	}
-
+	checkEps(mns);
 	vector <vector <double> > mis;
 	mis.resize(3);
 	for (int i = 0; i < 3; ++i){
@@ -78,6 +80,8 @@ double Gauss (const double l, const double r){
 			mis[i].push_back(M[i+j]);
 		}
 	}
+	checkEps(mis);
+
 	vector <vector <double>> P, L, U;
 	P.resize(3);
 	L.resize(3);
@@ -94,11 +98,17 @@ double Gauss (const double l, const double r){
 	vector <double> a;
 	a.resize(3);
 	copy(a, solveLinerSystem(P, L, U, mns));
+	checkEps(a);
+	//cout << "vector a: " << endl;
+	//print(a);
 
 	vector < double > x;
+	x.resize(3);
 	MethodNewton( (l+r)/2, a, x);
 	//findRootsCardano(a, x);
-
+	checkEps(x);
+	cout << "x: " << endl;
+	print(x);
 	vector <double> A;
 	A.resize(3);
 
@@ -109,6 +119,7 @@ double Gauss (const double l, const double r){
 			X[i].push_back(pow(x[j], i));
 		}
 	}
+	checkEps(X);
 
 	PLU(X, P, L, U);
 	vector <double> ms;
@@ -116,16 +127,32 @@ double Gauss (const double l, const double r){
 		ms.push_back(M[i]);
 	}
 	copy(A, solveLinerSystem(P,L, U, ms));
-
-	double res = A[0] * f(x[0]) + A[1] * f(x[1]) + A[2] * f(x[2]);
-	if (fabs(res) < EPS)
+	checkEps(A);
+	double res = 0.0;
+	res = A[0] * f(x[0]) + A[1] * f(x[1]) + A[2] * f(x[2]);
+	/*
+	if (fabs(A[0]) > EPS){
+		res += A[0] * f(x[0]);
+	}
+	if (fabs(A[1]) > EPS){
+		res += A[1] * f(x[1]);
+	}
+	if ( fabs (A[2]) > EPS){
+		res += A[2] * f(x[2]);
+	}
+	if (fabs (res) < EPS)
 		res = 0.0;
+	*/
+	//cout << "res" << res << endl;
 	return res;
 }
 
 double calculateIntegralWithStepG(const double l, const double r, const int h){
-	double res = 0;
+	double res = 0.0;
 	for (int i = 0; i < h; ++i){
+		if (i == 12 && h == 14){
+			//cout << "QQ" << endl;
+		}
 		res += Gauss(l + i * ((r - l) / h), l + (i + 1)*((r - l) / h));
 	}
 	return res;
@@ -139,13 +166,20 @@ double calculateIntegralG(const double l, const double r, const double n)
 	unsigned int i = 1;
 	do{
 		sh1 = calculateIntegralWithStepG(l, r, i);
+		cout << "SH1: " << setprecision(12) << sh1 << endl;
+		cout << " == " << endl;
 		sh2 = calculateIntegralWithStepG(l, r, 2 * i);
+		cout << "SH2: " << setprecision(12) << sh2 << endl;
+		cout << " == " << endl;
 		R = (sh2 - sh1) / (pow(L, n) - 1.0);
-		cout << sh2 << " - " << sh1 << endl;
+		cout << " ======= "<< endl;
 		++i;
+		if (i == 7){
+			cout << "QQ" << endl;
+		}
 	} while (fabs(R) > EPS);
 	cout << "Numbers of iterations: " << i - 1 << endl;
-	return sh2;
+	return sh1;
 }
 vector<double> calculateMomentsForCotes(const double z0, const double z1){
 	vector <double> res;
@@ -207,18 +241,17 @@ void MethodNewton(const double x0, const vector <double> a, vector <double>& x){
 		x1 -= w_f(prev, a) / w_d(prev, a);
 		prev = x1;
 
-	} while (fabs(w_f(x1, a) - w_f(prev, a)) > EPS);
+	} while (fabs(w_f(x1, a)) > EPS);
 
-	x.push_back(x1);
+	x[0] = x1;
 
 	double x2, x3;
 	double p = a[2] + x1;
-	double q = a[1] + x1*(a[2] + x0);
-	double D = pow(p,2) - 4*q;
-	x2 = ( -p - sqrt(D)) / 2.0;
-	x3 = ( -p + sqrt(D)) / 2.0;
-	x.push_back(x2);
-	x.push_back(x3);
+	double q = a[1] + x1*(a[2] + x1);
+	double D = p*p - 4*q;
+	x[1] = ( -p - sqrt(D)) / 2.0;
+	x[2] = ( -p + sqrt(D)) / 2.0;
+	
 	return;
 }
 double calculateIntegralWithStepNC(const double l, const double r, const double h){
@@ -245,13 +278,27 @@ double calculateIntegralNC(const double l, const double r, const double n){
 	cout << "Numbers oi iterations: " << i << endl;
 	return sh2;
 }
-void findRootsCardano(const vector <double> a, vector<double> & x){
-	double Q = (pow(a[0], 2) - 3 * a[1]) / 9;
-	double R = (2 * pow(a[0], 3) - 9 * a[0] * a[1] + 27 * a[2]) / 54;
-	double t = acos(R / sqrt(pow(Q, 3))) / 3;
-	x.push_back(-2 * sqrt(Q)*cos(t) - a[0] / 3),
-	x.push_back(-2 * sqrt(Q)*cos(t + (2 * pi / 3)) - a[0] / 3);
-	x.push_back(-2 * sqrt(Q)*cos(t - (2 * pi / 3)) - a[0] / 3);
+void findRootsCardano(const vector <double> a_v, vector<double> & x){
+	double a, b, c;
+	a = a_v[2];
+	b = a_v[1];
+	c = a_v[0];
+
+	double q, r, r2, q3;
+	q = (a*a - 3.*b) / 9.; r = (a*(2.*a*a - 9.*b) + 27.*c) / 54.;
+	r2 = r*r; q3 = q*q*q;
+	if (r2 < q3) {
+		double t = acos(r / sqrt(q3));
+		a /= 3.; 
+		q = -2.*sqrt(q);
+		x[0] = q*cos(t / 3.) - a;
+		x[1] = q*cos((t - M_2PI) / 3.) - a;
+		x[2] = q*cos((t + M_2PI) / 3.) - a;
+		return;
+	}
+	else {
+		cout << "Error! Complex roots" << endl;
+	}
 	return;
 }
 
@@ -280,13 +327,10 @@ double AitkenG(const double l, const double r){
 }
 double w_f ( const double x, const vector <double> a){
 	double res = pow(x, 3) + a[2] * pow(x, 2) + a[1] * x + a[0];
-
 	return res;
 }
 
 double w_d ( double x, vector <double> a){
-	double res = 3 * pow(x, 2) + 2 * a[0] * x + a[1];
-	if (fabs(res) < EPS)
-		res = 0.0;
+	double res = 3 * pow(x, 2) + 2 * a[2] * x + a[1];
 	return res;
 }
