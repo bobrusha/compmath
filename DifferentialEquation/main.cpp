0565fd8df2eef3b6f5aa2603955a6af6ad7cf6d8
Sasha <bobrova.aleksand@gmail.com>
2014-05-27 21:19:47 +0400

    it's done

--- DifferentialEquation/main.cpp
+++ DifferentialEquation/main.cpp
@@ -16,16 +16,21 @@ vector <double> solve(double, double, const double, const double, const int);
 vector <double> solveWithOpponent(double, double, double, double, const int);
 vector <double> getSolutionRungeBase(double, double, double, double, const int);
 vector <double> getSolutionRungeOpponent(double, double, double, double, const int);
-vector <double> getSolutionAutoStepBase(double, double, double, double, const int);
-vector <double> getSolutionAutoStepOpponent(double, double, double, double, const int);
+vector <double> getSolutionAutoStepBase(double, double, double, double);
+vector <double> getSolutionAutoStepOpponent(double, double, double, double);
 
 void print(const vector <double>);
 void copy(vector <double>&, const vector <double>);
 int main(){
+	cout.precision(9);
 	cout << "Base with Runge rule:" << endl;
 	print(getSolutionRungeBase(0, M_PI, B*M_PI, A*M_PI, 2));
 	cout << "Opponent with Runge rule:" << endl;
 	print(getSolutionRungeOpponent(0, M_PI, B*M_PI, A*M_PI, 2));
+	cout << "Base with auto step" << endl;
+	print(getSolutionAutoStepBase(0, M_PI, B*M_PI, A*M_PI));
+	cout << "Opponent with auto step" << endl;
+	print(getSolutionAutoStepOpponent(0, M_PI, B*M_PI, A*M_PI));
 
 	system("pause");
 	return 0;
@@ -127,6 +132,7 @@ vector <double> getSolutionRungeBase(const double l, const double r, double y0, 
 		//cout << err1 << "  " << err2 << endl << endl;
 		++i;
 	} while (fabs(err1) > eps || fabs(err2) > eps);
+	cout << "Numbers of iterations = " << i << endl;
 	return v2;
 }
 vector <double> getSolutionRungeOpponent(const double l, const double r, double y0, double y1, const int n){
@@ -144,16 +150,18 @@ vector <double> getSolutionRungeOpponent(const double l, const double r, double 
 		copy(v2, solveWithOpponent(l, r, y0, y1, steps));
 		//cout << i << endl;
 		//print(v2);
-		err1 = (v2[0] - v1[0])/3.;
-		err2 = (v2[1] - v1[1])/3.;
+		err1 = (v2[0] - v1[0]) / 3.;
+		err2 = (v2[1] - v1[1]) / 3.;
 		//cout << err1 << "  " << err2 << endl<<endl;
 		++i;
 	} while (fabs(err1) > eps || fabs(err2) > eps);
+	cout << "Numbers of iterations = " << i << endl;
 	return v2;
 }
-/*
-vector <double> getSolutionAutoStep(const double l, const double r, const double y0, const double y1 ){
-	const double eps = 10e-5;
+
+vector <double> getSolutionAutoStepBase(const double l, const double r, const double y0, const double y1 ){
+	const int k = 2;
+	const double eps = 10e-9;
 	int i = 0;
 
 	vector <double> y;
@@ -161,25 +169,110 @@ vector <double> getSolutionAutoStep(const double l, const double r, const double
 	y.push_back(y1);
 
 	double delta;
-	if (l > r)
+	if (fabs(l) > fabs(r))
 		delta = pow(1 / l, 3) + pow(sqrt(pow(f1(y), 2) + pow(f2(y), 2)), 3);
 	else
 		delta = pow(1 / r, 3) + pow(sqrt(pow(f1(y), 2) + pow(f2(y), 2)), 3);
-	double h = pow(eps / delta,1./3.);
+
+	double h = pow(eps / delta, 1. / 3.);
 	double xj = l;
 	int steps = (int)((r - l) / h) + 1;
-	vector <double> y1, y2, yj;
-	yj.push_back(y0);
-	yj.push_back(y1);
+	h = (r - l) / steps;
+
+	vector <double> v1, v2, vj;
+	v1.resize(2);
+	v2.resize(2);
+
+	vj.push_back(y0);
+	vj.push_back(y1);
+
+	double err, err1, err2;
 
 	while (xj < r){
 		++i;
+		copy(v1, solve(xj, xj + h, vj[0], vj[1], 1));
+		copy(v2, solve(xj, xj + h / 2., vj[0], vj[1], 1));
+		copy(v2, solve(xj + h / 2., xj + h, v2[0], v2[1], 1));
 
+		err1 = (v2[0] - v1[0]) / (1 - pow(2, -2));
+		err2 = (v2[1] - v1[1]) / (1 - pow(2, -2));
+		err = sqrt(pow(err1, 2) + pow(err2, 2));
+
+		if (fabs(err) >(eps * pow(2, k))){
+			h /= 2.;
+		}
+		else if (err <= (eps * pow(2, k)) && err > eps){
+			h /= 2.;
+			copy(vj, v2);
+		}
+		else if (err <= eps && err >= (eps / pow(2, k + 1)))
+			copy(vj, v1);
+		else{
+			h *= 2.;
+			copy(vj, v1);
+		}
+		xj += h;
 	}
+	cout << "Numbers of iterations = " << i << endl;
+	return vj;
 }
-*/
+vector <double> getSolutionAutoStepOpponent(const double l, const double r, const double y0, const double y1){
+	const int k = 2;
+	const double eps = 10e-9;
+	int i = 0;
 
+	vector <double> y;
+	y.push_back(y0);
+	y.push_back(y1);
 
+	double delta;
+	if (fabs(l) > fabs(r))
+		delta = pow(1 / l, 3) + pow(sqrt(pow(f1(y), 2) + pow(f2(y), 2)), 3);
+	else
+		delta = pow(1 / r, 3) + pow(sqrt(pow(f1(y), 2) + pow(f2(y), 2)), 3);
+
+	double h = pow(eps / delta, 1. / 3.);
+	double xj = l;
+	int steps = (int)((r - l) / h) + 1;
+	h = (r - l) / steps;
+
+	vector <double> v1, v2, vj;
+	v1.resize(2);
+	v2.resize(2);
+
+	vj.push_back(y0);
+	vj.push_back(y1);
+
+	double err, err1, err2;
+
+	while (xj < r){
+		++i;
+		copy(v1, solveWithOpponent(xj, xj + h, vj[0], vj[1], 1));
+		copy(v2, solveWithOpponent(xj, xj + h/2., vj[0], vj[1], 1));
+		copy(v2, solveWithOpponent(xj + h / 2., xj + h, v2[0], v2[1], 1));
+
+		err1 = (v2[0] - v1[0]) / (1 - pow(2,-2));
+		err2 = (v2[1] - v1[1]) / (1 - pow(2,-2));
+		err = sqrt(pow(err1, 2) + pow(err2, 2));
+
+		if (fabs(err) > (eps * pow(2, k))){
+			h /= 2.;
+		}
+		else if (err <= (eps * pow(2, k)) && err > eps){
+			h /= 2.;
+			copy(vj, v2);
+		}
+		else if (err <= eps && err >= (eps / pow(2, k + 1)))
+			copy(vj, v1);
+		else{
+			h *= 2.;
+			copy(vj, v1);
+		}
+		xj += h;
+	}
+	cout << "Numbers of iterations = " << i << endl;
+	return vj;
+}
 void print(const vector <double> x){
 	for (int i = 0; i < x.size(); ++i){
 		cout << x[i] << endl;
